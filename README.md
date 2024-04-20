a4f74c3bcdbff92144408a850cceee13c7be04db
Brandel Otero <daniel.otero128@gmail.com>
2024-04-19 19:41:24 -0500

    ADD(It was added component product review)

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
-# React + Vite
+# Product Hunt Clone
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Este proyecto es un clon de Product Hunt, una plataforma donde los usuarios pueden descubrir y compartir nuevos productos tecnológicos y startups.
 
-Currently, two official plugins are available:
+## Tabla de Contenidos
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+1. [Descripción](#descripción)
+2. [Características](#características)
+3. [Capturas de Pantalla](#capturas-de-pantalla)
+4. [Instalación](#instalación)
+5. [Uso](#uso)
+6. [Tecnologías Utilizadas](#tecnologías-utilizadas)
+7. [Contribución](#contribución)
+8. [Licencia](#licencia)
+
+## Descripción
+
+Este clon de Product Hunt ha sido desarrollado como parte de un proyecto de aprendizaje para mejorar las habilidades en desarrollo web con React y Firebase.
+
+## Características
+
+- Registro de usuarios con autenticación.
+- Publicación y visualización de productos.
+- Sistema de votación para productos.
+- Comentarios y discusiones sobre productos.
+- Funcionalidad de búsqueda y filtrado de productos.
+
+## Capturas de Pantalla
+
+Inserta aquí capturas de pantalla de tu aplicación para mostrar cómo se ve y algunas de sus características principales.
+
+## Instalación
+
+Para instalar y ejecutar este proyecto en tu máquina local, sigue estos pasos:
+
+1. Clona este repositorio en tu computadora:
+
+   ```bash
+   git clone https://github.com/tu-usuario/product-hunt-clone.git
--- src/product-hunt/components/ProductPost.jsx
+++ src/product-hunt/components/ProductPost.jsx
@@ -1,12 +1,95 @@
-export const ProductPost = () => {
 
+import React, { useState } from 'react';
+
+export const ProductPost = () => {
+    const [product, setProduct] = useState({
+        title: 'Example product',
+        description: 'Description of product',
+        publisher: 'El pitalero 1',
+        averageRating: 4.5,
+        images: ['https://via.placeholder.com/150'],
+        purchaseLink: 'https://www.example.com',
+        store: 'Tienda de ejemplo',
+    });
+
+    const [reviews, setReviews] = useState([
+        { id: 1, user: 'User1', rate: 4, content: 'Good quality' },
+        { id: 2, user: 'User2', rate: 5, content: 'Exelent quality' },
+    ]);
+
+    const [newReview, setNewReview] = useState({ id: 0, user: '', rate: 0, content: '' });
+
+    const handleSubmitReview = () => {
+        const userJSON = localStorage.getItem('user');
+        const userObj = JSON.parse(userJSON);
+
+        newReview.user  = userObj.userName;
+        setReviews([...reviews, newReview]);
+        console.log(reviews);
+    };
 
     return (
-        <>
-            <h1>ProductPostWorks</h1>
-        </>
-    )
+        <div className="container mt-4">
+            <div className="row">
+                <div className="col-2">
+                    <img src={product.images[0]} className="card-img-top img-fluid " alt="Producto" />
+                </div>
+                <div className='col-6'>
+                    <div className="card-body">
+                        <h5 className="card-title">{product.title}</h5>
+                        <p className="card-text py-3">{product.description}</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">Usuario: {product.publisher}</li>
+                        <li className="list-group-item">Promedio de calificación: {product.averageRating}</li>
+                    </ul>
+                    <div className="card-body py-2">
+                        <a href={product.purchaseLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Comprar en {product.store}</a>
+                    </div>
+                </div>
+            </div>
+            <div className="row mt-4">
+                <div className='col-lg-2'></div>
+                <div className="col-lg-8">
 
+                    <form>
+                        <h3>Deja tu reseña</h3>
+                        <div className="form-group">
+                            <label className='py-3' htmlFor="formRating">Calificación:</label>
+                            <select className="form-control" id="formRating" value={newReview.rate} onChange={(e) => setNewReview({ ...newReview, rate: e.target.value })}>
+                                <option value="0">Selecciona una calificación</option>
+                                <option value="1">1 estrella</option>
+                                <option value="2">2 estrellas</option>
+                                <option value="3">3 estrellas</option>
+                                <option value="4">4 estrellas</option>
+                                <option value="5">5 estrellas</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label className='py-3' htmlFor="formContent">Comentario:</label>
+                            <textarea className="form-control py-3" id="formContent" rows="3" value={newReview.content} onChange={(e) => setNewReview({ ...newReview, content: e.target.value })}></textarea>
+                        </div>
+                        <button type="button" className="btn btn-primary" onClick={handleSubmitReview}>Enviar reseña</button>
+                    </form>
+                </div>
+            </div>
+            <div className="row mt-4">
+                <div className='col-2'></div>
+                <div className="col-8">
+                    <h3>Reseñas de usuarios</h3>
+                    {reviews.map(review => (
+                        <div className="card mb-2" key={review.id}>
+                            <div className="card-body">
+                                <h5 className="card-title">{review.user}</h5>
+                                <p className="card-text">Calificación: {review.rate}</p>
+                                <p className="card-text">Comentario: {review.content}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 
 }
 
